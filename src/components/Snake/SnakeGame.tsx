19823dcfc9c71ee0a6ed66f07201cef8ac2d8a4f
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-04-08 09:28:20 +0200

    Improve Snake

--- src/components/Snake/SnakeGame.tsx
+++ src/components/Snake/SnakeGame.tsx
@@ -1,4 +1,10 @@
 import React, { useEffect } from "react";
+import apple from "../../img/svg/apple.svg"
+import styled from "styled-components";
+
+const HiddenApple = styled.img`
+  display: none;
+`
 
 const SnakeGame = () => {
     useEffect(() => {
@@ -10,6 +16,7 @@ const SnakeGame = () => {
 
     return (<>
         <canvas id={'gc'} width={'400'} height={'400'}/>
+        <HiddenApple id="appleId" src={apple} />
     </>)
 }
 
@@ -21,6 +28,9 @@ let appleX: number = 15
 let appleY: number = 15
 let xVelocity: number = 0
 let yVelocity: number = 0
+let paused: boolean = false
+let tempXVelocity = 0
+let tempYVelocity = 0
 
 let trail: any[] = [];
 let tail: number = 5;
@@ -51,12 +61,12 @@ const game = (context: any) => {
     context.fillStyle = "lime";
     for (let i = 0; i < trail.length; i++) {
         context.fillRect(trail[i].x * gameScreen, trail[i].y * gameScreen, gameScreen - 2, gameScreen - 2);
-        if (trail[i].x === playerX && trail[i].y === playerY) {
+        if (trail[i].x === playerX && trail[i].y === playerY && !paused) {
             tail = 5;
         }
     }
     trail.push({x: playerX, y: playerY});
-    while (trail.length > tail) {
+    while (trail.length > tail && !paused) {
         trail.shift();
     }
 
@@ -66,31 +76,54 @@ const game = (context: any) => {
         appleY = Math.floor(Math.random() * tc);
     }
 
-    context.fillStyle = "red";
-    context.fillRect(appleX * gameScreen, appleY * gameScreen, gameScreen - 2, gameScreen - 2);
+    const img = document.getElementById("appleId")
+    context.drawImage(img, appleX * gameScreen, appleY * gameScreen, gameScreen - 2, gameScreen - 2)
 }
 
 const keyPush = (evt: any) => {
     switch (evt.keyCode) {
         case 27:
-            xVelocity = 0;
-            yVelocity = 0;
+            console.log('27! Esc ', paused);
+            if (!paused) {
+                paused = true
+                tempXVelocity = xVelocity
+                tempYVelocity = yVelocity
+                xVelocity = 0;
+                yVelocity = 0
+            } else {
+                paused = false
+                console.log('X: ', tempXVelocity, ' Y: ', tempYVelocity);
+                xVelocity = tempXVelocity
+                yVelocity = tempYVelocity
+            }
             break;
         case 37:
-            xVelocity = -1;
-            yVelocity = 0;
+            console.log('37! Venstre ', paused);
+            if (xVelocity < 1 && !paused) {
+                xVelocity = -1;
+                yVelocity = 0;
+            }
             break;
         case 38:
-            xVelocity = 0;
-            yVelocity = -1;
+            console.log('38! Opp ', paused);
+            if (yVelocity < 1 && !paused) {
+                xVelocity = 0;
+                yVelocity = -1;
+            }
             break;
         case 39:
-            xVelocity = 1;
-            yVelocity = 0;
+            console.log('39! Høyre ', paused);
+            if (xVelocity > -1 && !paused) {
+                xVelocity = 1;
+                yVelocity = 0;
+            }
             break;
         case 40:
-            xVelocity = 0;
-            yVelocity = 1;
+            console.log('40! Ned ', paused);
+            if (yVelocity > -1 && !paused) {
+                xVelocity = 0;
+                yVelocity = 1;
+            }
             break;
     }
 }
