aee795fefbc88d26503a4375f182040f78a6422b
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-04-09 10:38:09 +0200

    Add buzzword to Homepage
    Tweak colors

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,11 +1,47 @@
 import Page from "../Page";
+import styled from "styled-components";
+import {Wave} from "react-animated-text";
 
 const texts = {
   title: "Velkommen!",
+  buzzwords: [
+    'Synergy',
+    'Sustainability',
+    'Innovation',
+    'Equality',
+    'Streamlined',
+  ],
 }
 
+const Buzzword = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1em;
+  font-size: 10em;
+`
+
 const Homepage = () => {
-  return (<Page title={texts.title} />)
+  return (<Page title={texts.title}>
+    <Buzzword>
+      <Wave
+        text={randomWord()}
+        speed={4}
+        iterations={1}
+        effect="verticalFadeIn"
+        effectChange={2}
+        effectDuration={0.7}
+      />
+    </Buzzword>
+  </Page>)
+}
+
+const randomWord = () => {
+  const randomIndex = Math.floor(Math.random() * texts.buzzwords.length);
+
+  return texts.buzzwords[randomIndex];
 }
 
 export default Homepage;
--- src/components/Themes.tsx
+++ src/components/Themes.tsx
@@ -10,23 +10,23 @@ export interface Theme {
 }
 
 export const lightTheme: Theme = {
-  body: '#F0F4F8',
-  text: '#102A43',
+  body: '#D9E2EC',
+  text: '#243B53',
   toggleButtonBackground: '#363537',
   button: {
-    background: '#D9E2EC',
-    text: '#243B53',
+    background: '#BCCCDC',
+    text: '#334E68',
     focusShadow: '#829AB1',
   }
 }
 
 export const darkTheme: Theme = {
-  body: '#102A43',
-  text: '#F0F4F8',
+  body: '#243B53',
+  text: '#D9E2EC',
   toggleButtonBackground: '#999',
   button: {
-    background: '#243B53',
-    text: '#D9E2EC',
+    background: '#334E68',
+    text: '#BCCCDC',
     focusShadow: '#627D98',
   }
 }
