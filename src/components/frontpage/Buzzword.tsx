bfe22ffe58896c0dcb912065ec466284619a2f49
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-04-12 12:36:27 +0200

    Rotate buzzwords infinitely

--- src/components/frontpage/Buzzword.tsx
+++ src/components/frontpage/Buzzword.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Wave } from "react-animated-text";
 import styled from "styled-components";
 import { randomElementFromArray } from "../../utils/arrayUtils";
@@ -17,11 +17,34 @@ const BuzzwordBox = styled.div`
   font-size: min(10vw, 10em);
 `;
 
+const getNewWord = (activeWord: string) => {
+  let newWord = activeWord;
+
+  // Make sure we never set the same word twice, because then the state wouldn't change and we'd exit the infinite useEffect loop
+  while (newWord === activeWord) {
+    newWord = randomElementFromArray(texts.buzzwords);
+  }
+  return newWord;
+};
+
 const Buzzword = () => {
+  const [activeWord, setActiveWord] = useState(
+    randomElementFromArray(texts.buzzwords)
+  );
+
+  // Infinite loop of re-rendering because we set state while listening to state changes, probably not a great idea
+  useEffect(() => {
+    setTimeout(() => {
+      setActiveWord(getNewWord(activeWord));
+    }, 5000);
+  });
+
   return (
-    <BuzzwordBox>
+    <BuzzwordBox
+      onClick={() => setActiveWord(randomElementFromArray(texts.buzzwords))}
+    >
       <Wave
-        text={randomElementFromArray(texts.buzzwords)}
+        text={activeWord}
         speed={4}
         iterations={1}
         effect="verticalFadeIn"
