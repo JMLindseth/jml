7c5c9989338915fa6439e08689198f137caea3c4
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-04-15 09:04:01 +0200

    Add points and game over screen

--- src/components/Snake/SnakeGame.tsx
+++ src/components/Snake/SnakeGame.tsx
@@ -12,12 +12,51 @@ const SmallDropdown = styled(Dropdown)`
   margin-left: auto;
 `;
 
+interface Trail {
+  x: number;
+  y: number;
+}
+
+interface GameVariables {
+  playerX: number;
+  playerY: number;
+  tileSize: number;
+  tileCount: number;
+  appleX: number;
+  appleY: number;
+  xVelocity: number;
+  yVelocity: number;
+  paused: boolean;
+  trail: Trail[];
+  tail: number;
+  score: number;
+  gameOver: boolean;
+}
+
+const defaultGameVariables: GameVariables = {
+  playerX: 10,
+  playerY: 10,
+  tileSize: 20,
+  tileCount: 400 / 20,
+  appleX: 15,
+  appleY: 15,
+  xVelocity: 0,
+  yVelocity: 0,
+  paused: false,
+  trail: [] as Trail[],
+  tail: 5,
+  score: 0,
+  gameOver: false
+};
+
+let gameVariables = {...defaultGameVariables};
+
 const SnakeGame = () => {
   const [size, setSize] = React.useState(400);
 
   const changeSize = (element: Option) => {
     setSize(parseInt(element.value));
-    tileCount = parseInt(element.value) / 20;
+    gameVariables.tileCount = parseInt(element.value) / 20;
   };
 
   useEffect(() => {
@@ -32,7 +71,7 @@ const SnakeGame = () => {
   const options: Option[] = [
     { value: "400", label: "liten" },
     { value: "600", label: "medium" },
-    { value: "1000", label: "stor" },
+    { value: "1000", label: "stor" }
   ];
 
   return (
@@ -48,32 +87,19 @@ const SnakeGame = () => {
   );
 };
 
-let playerX = 10;
-let playerY = 10;
-const tileSize = 20;
-let tileCount: number = 400 / 20;
-let appleX = 15;
-let appleY = 15;
-let xVelocity = 0;
-let yVelocity = 0;
-let paused = false;
-
-interface TrailType {
-  x: number;
-  y: number;
-}
-
-const trail: TrailType[] = [];
-let tail = 5;
-
 const game = (context: CanvasRenderingContext2D) => {
-  if (paused) {
+  if (gameVariables.gameOver) {
+    paintCanvasBlack(context);
+    displayGameOverScreen(context);
+    displayScore(context);
+  } else if (gameVariables.paused) {
     paintCanvasBlack(context);
     displayPauseText(context);
+    displayScore(context);
   } else {
     regenerateAppleIfOutsidePLayableArea(context);
-    playerX += xVelocity;
-    playerY += yVelocity;
+    gameVariables.playerX += gameVariables.xVelocity;
+    gameVariables.playerY += gameVariables.yVelocity;
 
     checkEdges();
 
@@ -82,6 +108,7 @@ const game = (context: CanvasRenderingContext2D) => {
     moveSnake(context);
 
     eatAndGenerateApple(context);
+    displayScore(context);
   }
 };
 
@@ -90,118 +117,148 @@ const paintCanvasBlack = (context: CanvasRenderingContext2D) => {
   context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 };
 
+const drawText = (context: CanvasRenderingContext2D, text: string, font?: string, fillStyle?: string, textAllign?: CanvasTextAlign, x?: number, y?: number) => {
+  context.font = font ? font : "30px Arial";
+  context.fillStyle = fillStyle ? fillStyle : "red";
+  context.textAlign = textAllign ? textAllign : "center";
+  const xCoordinate = x ? x : context.canvas.width / 2
+  const yCoordinate = y ? y : context.canvas.height / 2
+
+  context.fillText(
+    text,
+    xCoordinate,
+    yCoordinate
+  );
+}
+
 const displayPauseText = (context: CanvasRenderingContext2D) => {
   const pausedText = "PAUSED!";
 
-  context.font = "30px Arial";
-  context.fillStyle = "red";
-  context.textAlign = "center";
-  context.fillText(
-    pausedText,
-    context.canvas.width / 2,
-    context.canvas.height / 2
-  );
+  drawText(context, pausedText);
+};
+
+const displayGameOverScreen = (context: CanvasRenderingContext2D) => {
+  const gameOverText = "GAME OVER! :(";
+  const scoreText = `You got ${gameVariables.score} points! :D`
+  const newGameText = `Press Escape to start a new game`
+
+  drawText(context, gameOverText);
+  drawText(context, scoreText, "20px Arial", "green", "center", context.canvas.width / 2, (context.canvas.height / 2) + 40)
+  drawText(context, newGameText, "15px Arial", "white", "center", context.canvas.width / 2, (context.canvas.height / 2) + 70)
+};
+
+const displayScore = (context: CanvasRenderingContext2D) => {
+  const score = `${gameVariables.score}`;
+
+  drawText(context, score, "20px Arial", "green", "right", context.canvas.width - 10, 20)
 };
 
 const checkEdges = () => {
-  if (playerX < 0) {
-    playerX = tileCount - 1;
+  if (gameVariables.playerX < 0) {
+    gameVariables.playerX = gameVariables.tileCount - 1;
   }
 
-  if (playerX > tileCount - 1) {
-    playerX = 0;
+  if (gameVariables.playerX > gameVariables.tileCount - 1) {
+    gameVariables.playerX = 0;
   }
 
-  if (playerY < 0) {
-    playerY = tileCount - 1;
+  if (gameVariables.playerY < 0) {
+    gameVariables.playerY = gameVariables.tileCount - 1;
   }
 
-  if (playerY > tileCount - 1) {
-    playerY = 0;
+  if (gameVariables.playerY > gameVariables.tileCount - 1) {
+    gameVariables.playerY = 0;
   }
 };
 
 const regenerateAppleIfOutsidePLayableArea = (
   context: CanvasRenderingContext2D
 ) => {
-  if (appleX > tileCount || appleY > tileCount) {
-    appleX = Math.floor(Math.random() * tileCount);
-    appleY = Math.floor(Math.random() * tileCount);
+  if (gameVariables.appleX > gameVariables.tileCount || gameVariables.appleY > gameVariables.tileCount) {
+    gameVariables.appleX = Math.floor(Math.random() * gameVariables.tileCount);
+    gameVariables.appleY = Math.floor(Math.random() * gameVariables.tileCount);
 
     const img = document.getElementById("appleId") as CanvasImageSource;
     context.drawImage(
       img,
-      appleX * tileSize,
-      appleY * tileSize,
-      tileSize - 2,
-      tileSize - 2
+      gameVariables.appleX * gameVariables.tileSize,
+      gameVariables.appleY * gameVariables.tileSize,
+      gameVariables.tileSize - 2,
+      gameVariables.tileSize - 2
     );
   }
 };
 
 const moveSnake = (context: CanvasRenderingContext2D) => {
   context.fillStyle = "lime";
-  for (let i = 0; i < trail.length; i++) {
+  for (let i = 0; i < gameVariables.trail.length; i++) {
     context.fillRect(
-      trail[i].x * tileSize,
-      trail[i].y * tileSize,
-      tileSize - 2,
-      tileSize - 2
+      gameVariables.trail[i].x * gameVariables.tileSize,
+      gameVariables.trail[i].y * gameVariables.tileSize,
+      gameVariables.tileSize - 2,
+      gameVariables.tileSize - 2
     );
-    if (trail[i].x === playerX && trail[i].y === playerY) {
-      tail = 5;
+    if (gameVariables.trail[i].x === gameVariables.playerX && gameVariables.trail[i].y === gameVariables.playerY && gameVariables.score !== 0) {
+      gameVariables.gameOver = true;
+      break;
     }
   }
-  trail.push({ x: playerX, y: playerY });
-  while (trail.length > tail) {
-    trail.shift();
+  gameVariables.trail.push({ x: gameVariables.playerX, y: gameVariables.playerY });
+  while (gameVariables.trail.length > gameVariables.tail) {
+    gameVariables.trail.shift();
   }
 };
 
 const eatAndGenerateApple = (context: CanvasRenderingContext2D) => {
-  if (appleX === playerX && appleY === playerY) {
-    tail++;
-    appleX = Math.floor(Math.random() * tileCount);
-    appleY = Math.floor(Math.random() * tileCount);
+  if (gameVariables.appleX === gameVariables.playerX && gameVariables.appleY === gameVariables.playerY) {
+    gameVariables.tail++;
+    gameVariables.score++;
+    gameVariables.appleX = Math.floor(Math.random() * gameVariables.tileCount);
+    gameVariables.appleY = Math.floor(Math.random() * gameVariables.tileCount);
   }
 
   const img = document.getElementById("appleId") as CanvasImageSource;
   context.drawImage(
     img,
-    appleX * tileSize,
-    appleY * tileSize,
-    tileSize - 2,
-    tileSize - 2
+    gameVariables.appleX * gameVariables.tileSize,
+    gameVariables.appleY * gameVariables.tileSize,
+    gameVariables.tileSize - 2,
+    gameVariables.tileSize - 2
   );
 };
 
 const keyPush = (evt: KeyboardEvent) => {
   switch (evt.key) {
     case "Escape":
-      paused = !paused;
+      if (gameVariables.gameOver) {
+        gameVariables = {...defaultGameVariables}
+      }
+      else {
+        gameVariables.paused = !gameVariables.paused;
+      }
       break;
     case "ArrowLeft":
-      if (xVelocity < 1) {
-        xVelocity = -1;
-        yVelocity = 0;
+      if (gameVariables.xVelocity < 1) {
+        gameVariables.xVelocity = -1;
+        gameVariables.yVelocity = 0;
       }
       break;
     case "ArrowUp":
-      if (yVelocity < 1) {
-        xVelocity = 0;
-        yVelocity = -1;
+      if (gameVariables.yVelocity < 1) {
+        gameVariables.xVelocity = 0;
+        gameVariables.yVelocity = -1;
       }
       break;
     case "ArrowRight":
-      if (xVelocity > -1) {
-        xVelocity = 1;
-        yVelocity = 0;
+      if (gameVariables.xVelocity > -1) {
+        gameVariables.xVelocity = 1;
+        gameVariables.yVelocity = 0;
       }
       break;
     case "ArrowDown":
-      if (yVelocity > -1) {
-        xVelocity = 0;
-        yVelocity = 1;
+      if (gameVariables.yVelocity > -1) {
+        gameVariables.xVelocity = 0;
+        gameVariables.yVelocity = 1;
       }
       break;
   }
