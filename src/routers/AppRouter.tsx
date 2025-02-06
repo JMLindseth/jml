5d26625755cabaaa51ad39ce4565ef228a1c4030
John Martin Lindseth <john.martin.lindseth@nav.no>
2025-02-06 12:41:49 +0100

    Upgrade react router to v6

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Frontpage from "../components/frontpage/Frontpage";
 import SnakePage from "../components/Snake/SnakePage";
 import CodeboxPage from "../components/Codebox/CodeboxPage";
@@ -8,12 +8,12 @@ import MiscellaneousPage from "../components/Miscellaneous/MiscellaneousPage";
 const AppRouter = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/snake" component={SnakePage} />
-        <Route path="/codebox" component={CodeboxPage} />
-        <Route path="/miscellaneous" component={MiscellaneousPage} />
-        <Route path="/" component={Frontpage} />
-      </Switch>
+      <Routes>
+        <Route path="/snake" element={<SnakePage/>}/>
+        <Route path="/codebox" element={<CodeboxPage/>}/>
+        <Route path="/miscellaneous" element={<MiscellaneousPage/>}/>
+        <Route path="/" element={<Frontpage/>}/>
+      </Routes>
     </Router>
   );
 };
