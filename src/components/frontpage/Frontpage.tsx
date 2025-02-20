f9cd4583c5c0bc3ee5a51bc3fa9135a022a73284
JMLindseth <john.martin.lindseth@nav.no>
2025-02-20 15:14:28 +0100

    Fjern Buzzword

--- src/components/frontpage/Frontpage.tsx
+++ src/components/frontpage/Frontpage.tsx
@@ -1,32 +1,12 @@
 import React from "react";
 import Page from "../../Page";
-import styled from "styled-components";
 import { useRef } from "react";
-import Buzzword from "./Buzzword";
-
-const ContentBlock = styled.div`
-  min-height: 100vmin;
-  padding: 25vmin 5vmin;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  overflow: hidden;
-
-  > * {
-    max-width: 100%;
-  }
-`;
 
 const Frontpage = () => {
   const ref = useRef<HTMLDivElement>(null);
 
   return (
-    <Page>
-      <ContentBlock ref={ref}>
-        <Buzzword />
-      </ContentBlock>
-    </Page>
+    <Page />
   );
 };
 
--- src/utils/arrayUtils.ts
+++
@@ -1,5 +0,0 @@
-export const randomElementFromArray = <T>(array: T[]) => {
-  const randomIndex = Math.floor(Math.random() * array.length);
-
-  return array[randomIndex];
-};
