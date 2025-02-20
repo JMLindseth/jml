fdfb471c585d93be572ac6bc3f1faf434d50568b
JMLindseth <john.martin.lindseth@nav.no>
2025-02-20 15:04:29 +0100

    Fjern quotes

--- src/components/frontpage/Frontpage.tsx
+++ src/components/frontpage/Frontpage.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import Page from "../../Page";
 import styled from "styled-components";
 import { useRef } from "react";
-import DoubleArrowDown from "../../img/doubleArrowDown";
-import Intro from "./Intro";
 import Buzzword from "./Buzzword";
-import Quote from "./Quote";
 
 const ContentBlock = styled.div`
   min-height: 100vmin;
@@ -21,41 +18,13 @@ const ContentBlock = styled.div`
   }
 `;
 
-const ArrowButton = styled.button`
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  margin-top: 10em;
-`;
-
-const ColoredArrow = styled(DoubleArrowDown)`
-  fill: ${({ theme }) => theme.text};
-
-  &:hover {
-    padding-top: 0.5em;
-  }
-`;
-
 const Frontpage = () => {
   const ref = useRef<HTMLDivElement>(null);
-
-  const onClickArrow = () => {
-    const top = ref.current?.getBoundingClientRect().height;
-    return window.scrollTo({ top: top, behavior: "smooth" });
-  };
 
   return (
     <Page>
       <ContentBlock ref={ref}>
         <Buzzword />
-        <ArrowButton tabIndex={-1} onClick={onClickArrow} aria-hidden>
-          <ColoredArrow />
-        </ArrowButton>
-      </ContentBlock>
-
-      <ContentBlock>
-        <Intro />
-        <Quote />
       </ContentBlock>
     </Page>
   );
