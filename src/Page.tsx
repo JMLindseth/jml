4354540fe11b7a556e059ed62689f8374c60ffc9
JMLindseth <john.martin.lindseth@nav.no>
2025-07-10 14:17:44 +0200

    Fjern Themes og flytt global css til index.css
    Vi trenger ikke GlobalStyles når vi ikke skal bytte mellom flere globale stylinger.

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,8 +1,5 @@
 import React, { ReactNode } from "react";
 import styled from "styled-components";
-import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./components/Themes";
-import { GlobalStyles } from "./components/Globalstyle";
 
 interface PageProps {
   title?: string;
@@ -16,36 +13,22 @@ const PageWrapper = styled.div`
   align-items: center;
 `;
 
-const Header = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
 const MainContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const fullTitle = (title?: string) => {
-  return title ? `${title} - JM Lindseth` : "JM Lindseth";
-};
-
 const Page = (pageProps: PageProps) => {
   const { title, children } = pageProps;
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <>
-        <GlobalStyles />
-        <PageWrapper>
-          <MainContent>
-            <h1>{title}</h1>
-            {children}
-          </MainContent>
-        </PageWrapper>
-      </>
-    </ThemeProvider>
+    <PageWrapper>
+      <MainContent>
+        <h1>{title}</h1>
+        {children}
+      </MainContent>
+    </PageWrapper>
   );
 };
 
